d3466ba4359a6486218836ad1659d0b02a8d45ea
NikunjMWebito <156300759+NikunjMWebito@users.noreply.github.com>
2025-07-04 10:25:38 +0530

    Set LocalMongo Connection

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,17 +1,17 @@
 import { MongoClient } from 'mongodb';
 
-const uri = 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const dbName = 'chatai';
+
+if (!uri) {
+    throw new Error('MONGODB_URI is not set and no fallback found');
+}
 
 let client;
 let clientPromise;
 
 if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
 }
 
